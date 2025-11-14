3a0bc67c0323da68e6321d833eb3acdfa65bff6f
altheyuhh <148861039+Althea420@users.noreply.github.com>
2025-11-14 12:41:04 +0800

    Add files via upload

---
+++ WS101_ESTORES_234596_TASK_1/Task3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        int sum = 0;
+         System.out.println("Enter 5 numbers: ");
+         for (int i = 0; i<5;i++){
+            numbers[i]= sc.nextInt();
+         } 
+         for (int num : numbers){
+            sum+=num;
+         }
+         double average = (double)sum/numbers.length;
+
+         System.out.println("Sum =" + sum);
+         System.out.println("Average = " + average);
+
+         sc.close();
+    }
+}
